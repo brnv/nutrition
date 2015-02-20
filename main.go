1f3355a0a009da6562307edc974c2b615eb776bb
Alexey Baranov <a.baranov@office.ngs.ru>
2015-02-20 13:43:30 +0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+//@TODO: reading from configuration file
+//	./cmd settings show
+
+//@TODO: update configuration file with new data
+//	./cmd settings set <entry> <value>
+
+//@TODO: command line tool interface
+//	./cmd add <product>
+//	./cmd check <product> <weight>
+//	./cmd eat (breakfast|lunch|snack|dinner) <product> <weight>
+//  ./cmd journal
+//  ./cmd journal (today)
