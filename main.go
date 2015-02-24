cc1ad8ec7d2315c0eb8e309d890eb423e22589f7
Alexey Baranov <a.baranov@office.ngs.ru>
2015-02-24 18:49:49 +0600

    product impact calculation

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"strconv"
+	"text/template"
 
 	"github.com/docopt/docopt.go"
 	"github.com/op/go-logging"
+	"github.com/seletskiy/tplutil"
 )
 
 const configFilename = "./config.toml"
@@ -20,6 +24,7 @@ const usage = `
 	nutrition settings show
 	nutrition settings set <entry> <value>
 	nutrition product add <product>
+	nutrition check <product> <weight>
 `
 
 var config Config
@@ -72,6 +77,57 @@ func main() {
 			}
 
 			os.Exit(0)
+		}
+	}
+
+	if args["check"].(bool) {
+		weightFloat, err := strconv.ParseFloat(args["<weight>"].(string), 64)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		checkProductImpact(
+			args["<product>"].(string),
+			weightFloat,
+		)
+	}
+}
+
+//@TODO: move calculations to another file
+
+const productImpactTpl = `
+	carbohydrates = {{.Carbohydrates}}%{{"\n"}}
+	proteins = {{.Proteins}}%{{"\n"}}
+	fats = {{.Fats}}%{{"\n"}}
+	calories = {{.Calories}}%{{"\n"}}
+`
+
+func checkProductImpact(productName string, weight float64) {
+	products, _ := productsRead(productsFile)
+
+	for _, product := range products.Product {
+		if product.Name == productName {
+			impact := struct {
+				Carbohydrates float64
+				Proteins      float64
+				Fats          float64
+				Calories      float64
+			}{
+				Carbohydrates: product.Carbohydrates * weight / 100 / config.Settings.Carbohydrates * 100,
+				Proteins:      product.Proteins * weight / 100 / config.Settings.Proteins * 100,
+				Fats:          product.Fats * weight / 100 / config.Settings.Fats * 100,
+				Calories:      product.Calories * weight / 100 / config.Settings.Calories * 100,
+			}
+
+			myTpl := template.Must(
+				template.New("productImpactTpl").Parse(tplutil.Strip(
+					productImpactTpl,
+				)))
+
+			buf := bytes.NewBuffer([]byte{})
+			myTpl.Execute(buf, impact)
+
+			fmt.Print(buf.String())
 		}
 	}
 }
